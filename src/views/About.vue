0f7cf80c9626956ef26fed39bf969af9f9012275
John Lynn <john@otter.tech>
2019-12-24 11:12:29 -0800

    Update 'About' page.

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About VCounter</h1>
+    <p>
+      VCounter is the continuation of the original
+      <a href>NCounter</a> project. It's purpose is to give the more tedius parts of pen and paper games to a computer so that the players can focus on playing.
+    </p>
+    <p>
+      If you'd like to see a new feature added, a bug fixed, or just want to say "Hi", go ahead and create an issue on
+      <a href="https://github.com/madigan/vcounter/issues">GitHub</a>!
+    </p>
+    <p></p>
   </div>
 </template>
