cdd6b2c85759ab20ffe26efcdc0bcefd1356a988
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-28 03:39:54 +0000

    Deploy madigan/vcounter to github.com/madigan/vcounter.git:gh-pages

---
+++ precache-manifest.20d6c406f470d89f5dfd838596836e26.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77190fcc3d445edad993",
+    "url": "/vcounter/css/app.44442965.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vcounter/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "4956bc0506c677fe368761ab4e50ce5b",
+    "url": "/vcounter/index.html"
+  },
+  {
+    "revision": "d5dcf3d8a05def528358",
+    "url": "/vcounter/js/about.53f4d4e7.js"
+  },
+  {
+    "revision": "77190fcc3d445edad993",
+    "url": "/vcounter/js/app.c58d2f4a.js"
+  },
+  {
+    "revision": "5ed44fe0212354e75a6c",
+    "url": "/vcounter/js/chunk-vendors.fb38c3d0.js"
+  },
+  {
+    "revision": "9602c81bfaea14002bcabe10b350dd3d",
+    "url": "/vcounter/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vcounter/robots.txt"
+  }
+]);
